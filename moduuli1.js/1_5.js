3337fb1e919ef746d19e3a166399ab86b8b811ad
munttu <muntass@metropolia.fi>
2024-11-10 03:44:57 +0200

    m30w

--- moduuli1.js/1_5.js
+++ moduuli1.js/1_5.js
@@ -1,2 +1,11 @@
-let year = parseInt(prompt('input year to check if leap year'));
-if (year / )
+let year = parseInt(prompt('Input year to check if it is a leap year'));
+
+if (year % 400 === 0) {
+    console.log(year + " is a leap year");
+} else if (year % 100 === 0) {
+    console.log(year + " is not a leap year");
+} else if (year % 4 === 0) {
+    console.log(year + " is a leap year");
+} else {
+    console.log(year + " is not a leap year");
+}
